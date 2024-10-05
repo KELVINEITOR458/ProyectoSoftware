b22ff9e8a1fbc5bd807fcb236563bdd1ef39744c
Bgit70 <bryanlife2530@gmail.com>
2024-10-05 14:24:21 -0500

    clases creadas

---
+++ Estimaciones_App/src/app/components/model/costs.ts
@@ -0,0 +1,7 @@
+export class costs{
+    aditionalCosts!:string
+    totalCost!:string
+    monthlySalary!:string
+    finalCost!:string
+    utility!:string
+}
---
+++ Estimaciones_App/src/app/components/model/proyect.ts
@@ -0,0 +1,4 @@
+export class Proyect{
+    name!:string
+    timeProyect!:string
+}
